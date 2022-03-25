a0904bec3e04d843c1d38f9a4df1ac2c967b859a
Michał Derkacz <michal@Lnet.pl>
2022-03-25 17:41:42 +0100

    add flashloader.sh script

---
+++ devboard/fet1061/examples/flashloader.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# USB
+#interface='-u'
+
+# UART using /dev/ttyACM0
+interface='-p /dev/ttyACM0'
+
+sdphost $interface -- write-file 0x20000000 ivt_flashloader.bin
+sleep 1
+sdphost $interface -- jump-address 0x20000400
+sleep 1
+blhost  $interface -- get-property 1
+blhost  $interface -- fill-memory 0x2000 4 0xC0000006
+blhost  $interface -- configure-memory 9 0x2000
+blhost  $interface -- flash-image $1 erase 9
