a648f0b5f286fd5ae99851ebeb1ae241d4059ecc
Michał Derkacz <michal@Lnet.pl>
2022-07-12 19:50:14 +0200

    use emgo build instead of scripts/build.sh

--- devboard/fet1061/examples/build.sh
+++ devboard/fet1061/examples/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-GOTARGET=imxrt1060
-
 # FLEX_RAM: 512 KiB can assigned to:
 # - FLEX_ITCM  at 0x00000000
 # - FLEX_DTCM  at 0x20000000
@@ -9,11 +7,12 @@ GOTARGET=imxrt1060
 # in 32 KiB chunks.
 # OCRAM: 512 KiB at 0x20200000
 
-GOTEXT=0x60002000
-GOMEM=0x20200000:512K
-
-ISRNAMES=no
+export GOTARGET=imxrt1060
+export GOTEXT=0x60002000
+export GOMEM=0x20200000:512K
+export GOOUT=bin
+export ISRNAMES=no
 
 name=$(basename $(pwd))
 
-. $(emgo env GOROOT)/../scripts/build.sh $@ && objcopy -O binary $name.elf $name.bin && cat ../mbr.img $name.bin >$name+mbr.bin
+emgo build $@ && cat ../mbr.img $name.bin >$name+mbr.bin
