fca1e5d18e9af6a6d84c1733746a2c7e3cb1a75c
Michał Derkacz <michal@Lnet.pl>
2022-07-21 17:35:14 +0200

    examples/blinky: use constant names instead numbers when configuring PFD

--- devboard/fet1061/examples/blinky/main.go
+++ devboard/fet1061/examples/blinky/main.go
@@ -26,23 +26,35 @@ func main() {
 	// performance of analog blocks.
 	CCMA.MISC0_SET.Store(ccm_analog.MISC0_REFTOP_SELFBIASOFF)
 
+	gateAll := ccm_analog.PFD0_CLKGATE | ccm_analog.PFD1_CLKGATE |
+		ccm_analog.PFD2_CLKGATE | ccm_analog.PFD3_CLKGATE
+
 	// Setup PLL2
-	CCMA.PFD_528_SET.Store(ccm_analog.PFD0_CLKGATE | ccm_analog.PFD1_CLKGATE |
-		ccm_analog.PFD2_CLKGATE | ccm_analog.PFD3_CLKGATE)
-	CCMA.PFD_528.Store(0x2018101B) // 352,594,396,297 MHz
+	CCMA.PFD_528_SET.Store(gateAll)
+	CCMA.PFD_528.Store(0 |
+		27<<ccm_analog.PFD0_FRACn | // 528 MHz * 18 / 27 = 352 MHz
+		16<<ccm_analog.PFD1_FRACn | // 528 MHz * 18 / 16 = 594 MHz
+		24<<ccm_analog.PFD2_FRACn | // 528 MHz * 18 / 24 = 396 MHz
+		32<<ccm_analog.PFD3_FRACn, //  528 MHz * 18 / 32 = 297 MHz
+	)
 
 	// Setup PLL3
-	CCMA.PFD_480_SET.Store(ccm_analog.PFD0_CLKGATE | ccm_analog.PFD1_CLKGATE |
-		ccm_analog.PFD2_CLKGATE | ccm_analog.PFD3_CLKGATE)
-	CCMA.PFD_480.Store(0x13110D0C) // PFD0,1,2,3: 720,664,508,454 MHz
+	CCMA.PFD_480_SET.Store(gateAll)
+	CCMA.PFD_480.Store(0 |
+		12<<ccm_analog.PFD0_FRACn | // 480 MHz * 18 / 12 = 720 MHz
+		13<<ccm_analog.PFD1_FRACn | // 480 MHz * 18 / 13 = 665 MHz
+		17<<ccm_analog.PFD2_FRACn | // 480 MHz * 18 / 17 = 508 MHz
+		19<<ccm_analog.PFD3_FRACn, //  480 MHz * 18 / 19 = 455 MHz
+	)
 
 	CCM := ccm.CCM()
 
-	// Configure clocks
+	// Use OSC_CLK/1 = 24 MHz clock source for GPT and PIT timers.
 	CCM.CSCMR1.StoreBits(
 		ccm.PERCLK_PODF|ccm.PERCLK_CLK_SEL,
 		ccm.PERCLK_PODF_1|ccm.PERCLK_CLK_SEL,
 	)
+	// Use OSC_CLK/1 = 24 MHz clock source for UART.
 	CCM.CSCDR1.StoreBits(
 		ccm.UART_CLK_PODF|ccm.UART_CLK_SEL,
 		ccm.UART_CLK_PODF_1|ccm.UART_CLK_SEL,
@@ -50,8 +62,8 @@ func main() {
 
 	IOMUXC_GPR := iomuxc_gpr.IOMUXC_GPR()
 
-	// GPIO_MUX1 ALT5 selects GPIO6 (fast) instead of GPIO1 (slow) for all bits.
-	IOMUXC_GPR.GPR26.Store(0xFFFFFFFF)
+	// GPIO_MUX1 ALT5 selects GPIO6 (fast) instead of GPIO1 (slow) for bit 9.
+	IOMUXC_GPR.GPR26.Store(1 << 9)
 
 	IOMUXC := iomuxc.IOMUXC()
 
@@ -63,6 +75,7 @@ func main() {
 	IOMUXC.SW_PAD_CTL_PAD_GPIO_AD_B0_09.Store(iomuxc.DSE_7_R0_7)
 
 	rtos.SetPrivLevel(privLevel)
+	runtime.UnlockOSThread()
 
 	GPIO6 := gpio.GPIO6()
 
