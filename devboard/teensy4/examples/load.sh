f50f384443a415cc07637e4c09e9ae5eafb49dcc
Michał Derkacz <michal@Lnet.pl>
2022-08-03 18:31:32 +0200

    teensy4/examples/load.sh: add notice about teensy_loader_cli fail

--- devboard/teensy4/examples/load.sh
+++ devboard/teensy4/examples/load.sh
@@ -2,4 +2,12 @@
 
 name=$(basename $(pwd))
 
-teensy_loader_cli --mcu=TEENSY41 -v $name+mbr.hex
+teensy_loader_cli --mcu=TEENSY41 -v $name+mbr.hex || cat <<EOT
+teensy_loader_cli may fail on the first attempt. Check all connections and try again.
+
+See also:
+
+https://forum.pjrc.com/threads/67232-Teensy-4-What-triggers-Halfkay-to-do-a-full-flash-erase
+https://forum.pjrc.com/threads/69236-TeensyLoader-CLI-issues
+
+EOT
