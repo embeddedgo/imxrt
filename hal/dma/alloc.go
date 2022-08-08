61a5424a18e37bcabaf884dcbd19c3d93055ae14
Michał Derkacz <michal@Lnet.pl>
2022-08-09 01:41:56 +0200

    hal/dma: cache aligned allocation

---
+++ hal/dma/alloc.go
@@ -0,0 +1,23 @@
+// Copyright 2022 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dma
+
+import "unsafe"
+
+const cacheLineSize = 32 // Cortex-M7
+
+const CacheLineSize = cacheLineSize
+
+// Alloc works like make([]T, n) but guarantees that returned slice does not
+// share the same line in data cache with any other variable.
+func Alloc[T any](n int) (cacheAligned []T) {
+	size := unsafe.Sizeof(cacheAligned[0]) * uintptr(n)
+	size = (size + (cacheLineSize - 1)) &^ (cacheLineSize - 1)
+	size += cacheLineSize // extra space for address alignment
+	buf := make([]byte, size)
+	addr := uintptr(unsafe.Pointer(&buf[0]))
+	addr = (addr + (cacheLineSize - 1)) &^ (cacheLineSize - 1)
+	return unsafe.Slice((*T)(unsafe.Pointer(addr)), n)
+}
