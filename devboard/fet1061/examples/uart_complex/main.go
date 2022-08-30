8d5eccb50e5d90827a912f5f4583f16d3853af05
Michał Derkacz <michal@Lnet.pl>
2022-08-30 16:58:42 +0200

    hal/lpuart/lpuart1: new package

--- devboard/fet1061/examples/uart_complex/main.go
+++ devboard/fet1061/examples/uart_complex/main.go
@@ -2,16 +2,22 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Uart demonstrates how to use LPUART peripheral.
+// Uart-details demonstrates how to use LPUART peripheral using hal/lpuart
+// package directly.
 package main
 
 import (
+	"embedded/rtos"
 	"fmt"
 
+	"github.com/embeddedgo/imxrt/devboard/fet1061/board/leds"
 	"github.com/embeddedgo/imxrt/devboard/fet1061/board/pins"
+	"github.com/embeddedgo/imxrt/hal/dma"
+	"github.com/embeddedgo/imxrt/hal/irq"
 	"github.com/embeddedgo/imxrt/hal/lpuart"
-	"github.com/embeddedgo/imxrt/hal/lpuart/lpuart1"
 )
+
+var u *lpuart.Driver
 
 func main() {
 	// Used IO pins
@@ -19,10 +25,11 @@ func main() {
 	tx := pins.P24
 
 	// Setup LPUART driver
-	u := lpuart1.Driver()
+	u = lpuart.NewDriver(lpuart.LPUART(1), dma.Channel{}, dma.Channel{})
 	u.Setup(lpuart.Word8b, 115200)
 	u.UsePin(rx, lpuart.RXD)
 	u.UsePin(tx, lpuart.TXD)
+	irq.LPUART1.Enable(rtos.IntPrioLow, 0)
 
 	// Enable both directions
 	u.EnableRx(64) // use 64 byte ring buffer
@@ -38,4 +45,10 @@ func main() {
 		}
 		fmt.Fprintf(u, "%d: %s\r\n", n, buf[:n])
 	}
+}
+
+//go:interrupthandler
+func LPUART1_Handler() {
+	leds.User.Toggle() // visualize UART interrupts
+	u.ISR()
 }
