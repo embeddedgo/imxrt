01f608cbc96eb03cd06266f9d745d1ab17db0637
Michał Derkacz <michal@Lnet.pl>
2025-07-18 17:03:31 +0200

    cleanups...

--- devboard/teensy4/examples/mkmbr.sh
+++ devboard/teensy4/examples/mkmbr.sh
@@ -3,4 +3,4 @@
 FLASH_SIZE=8192 # KiB, 2048 in case of Teensy 4.0
 FLEXRAM_BANK_CFG=0x5555_5556 # 480 KiB OCRAM, 32 KiB DTCM
 
-imxmbr -flash $FLASH_SIZE -flexram $FLEXRAM_BANK_CFG mbr.img
+egtool imxmbr -flash $FLASH_SIZE -flexram $FLEXRAM_BANK_CFG mbr.img
