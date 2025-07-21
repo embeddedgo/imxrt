2ea3b8a0b88514780fd1f736a8938e70fb47d378
Michał Derkacz <michal@Lnet.pl>
2025-07-21 15:31:21 +0200

    p: regenerate  using svd/gen.sh

--- svd/gen.sh
+++ svd/gen.sh
@@ -12,7 +12,7 @@ svdxgen github.com/embeddedgo/imxrt/p ../svd/*.svd
 for p in aipstz ccm ccm_analog dma dmamux gpio iomuxc iomuxc_gpr ocotp pmu src lpi2c lpspi lpuart usb usb_analog usbphy wdog; do
 	cd $p
 	xgen -g *.go
-	GOTOOLCHAIN=go1.24-embedded GOOS=noos GOARCH=thumb go build -tags imxrt1060
+	GOTOOLCHAIN=go1.24.5-embedded GOOS=noos GOARCH=thumb go build -tags imxrt1060
 	cd ..
 done
 
